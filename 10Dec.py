9639b0c0ca55dea0142bfa75fee0da24ec1f3019
Anna <annamartinez0212@gmail.com>
2020-12-10 18:28:25 +0000

    10Dec - part 1

--- 10Dec.py
+++ 10Dec.py
@@ -6,11 +6,9 @@ numbers.sort(reverse = True)
 deviceJlt = max(numbers) + 3
 diffs = [min(numbers)]
 acc = deviceJlt
-# Any given adapter can take an input 1, 2, or 3 jolts lower than its rating and still produce its rated output joltage.
 
 for n in numbers:
   diff = acc - n
-
   diffs.append(diff)
   acc = n
 
